6eda13659af8b7acae75f8104be00180cf01db7d
vagrolina <160363487+vagrolina@users.noreply.github.com>
2026-06-29 01:58:39 +0300

    [ts-migrate][.] Init tsconfig.json file
    Co-authored-by: ts-migrate <>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
 
-export default [
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  pluginJs.configs.recommended,
-  eslintPluginPrettierRecommended
-]
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  tseslint.configs.recommended,
+]);
